beebf3e5a8866f9371fa8911714ca1c8cb238fc3
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-07-22 21:26:32 +0600

    Coustomize admin profile page and show user data

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -1,7 +1,96 @@
 @extends('admin.admin_dashboard')
 
 @section('admin')
+    <div class="page-content">
 
-<h2>Admin Profile</h2>
+        <div class="row profile-body">
+            <!-- left wrapper start -->
+            <div class="d-none d-md-block col-md-5 col-xl-4 left-wrapper">
+                <div class="card rounded">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-2">
+                            <div>
+                                <img class="wd-55 rounded-circle"
+                                    src="{{ !empty($profileData->photo) ? url('upload/admin_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
+                                    alt="profile">
+                                <span
+                                    class="h4 ms-3">{{ !empty($profileData->username) ? $profileData->username : 'No UserName' }}</span>
+                            </div>
+                        </div>
+                        <p>Hi! I'm Amiah the Senior UI Designer at NobleUI. We hope you enjoy the design and
+                            quality of Social.</p>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Joined:</label>
+                            <p class="text-muted">{{ !empty($profileData->created_at) ? $profileData->created_at : 'No Joining Date' }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Lives:</label>
+                            <p class="text-muted">{{ !empty($profileData->address) ? $profileData->address : 'No Address' }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Email:</label>
+                            <p class="text-muted">{{ !empty($profileData->email) ? $profileData->email : 'No Email Address' }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Phone:</label>
+                            <p class="text-muted">{{ !empty($profileData->phone) ? $profileData->phone : 'No Phone Number' }}</p>
+                        </div>
+                        <div class="mt-3">
+                            <label class="tx-11 fw-bolder mb-0 text-uppercase">Website:</label>
+                            <p class="text-muted">{{ !empty($profileData->website) ? $profileData->website : 'No Website' }}</p>
+                        </div>
+                        <div class="mt-3 d-flex social-links">
+                            <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                                <i data-feather="github"></i>
+                            </a>
+                            <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                                <i data-feather="twitter"></i>
+                            </a>
+                            <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                                <i data-feather="instagram"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- left wrapper end -->
+            {{-- Basic From Start  --}}
+            <div class="col-md-7 col-xl-8 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
 
+                        <h3 class="card-title">Basic Form</h3>
+
+                        <form class="forms-sample">
+                            <div class="mb-3">
+                                <label for="exampleInputUsername1" class="form-label">Username</label>
+                                <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off"
+                                    placeholder="Username">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off"
+                                    placeholder="Password">
+                            </div>
+                            <div class="form-check mb-3">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">
+                                    Remember me
+                                </label>
+                            </div>
+                            <button type="submit" class="btn btn-primary me-2">Submit</button>
+                            <button class="btn btn-secondary">Cancel</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            {{-- Basic From Start  --}}
+        </div>
+
+    </div>
 @endsection
