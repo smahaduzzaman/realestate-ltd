1d0792d77aa6f813a2e4163c52e1f202d3b28186
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-08-04 03:10:51 +0600

    Role & Permission Crud Completed

--- resources/views/backend/pages/permission/edit_permission.blade.php
+++ resources/views/backend/pages/permission/edit_permission.blade.php
@@ -13,6 +13,8 @@ Admin Chage Password
                         <form class="forms-sample" method="POST"
                             action="{{ route('update.permission', $permission->id) }}') }}">
                             @csrf
+                            @method('PUT')
+                            <input type="hidden" name="id" value="{{$permission->id}}">
                             <div class="mb-3">
                                 <label for="name" class="form-label">Permission Name</label>
                                 <input type="text" name="name" value="{{ $permission->name }}"
@@ -27,21 +29,24 @@ Admin Chage Password
                                 <select value="{{ $permission->group_name }}" name="group_name"
                                     class="form-control @error('group_name') is-invalid @enderror" id="group_name">
                                     <option selected disabled>Select Group</option>
-                                    <option value="property">Property Type</option>
-                                    <option value="state">State</option>
-                                    <option value="amenities">Amenities</option>
-                                    <option value="property">Property</option>
-                                    <option value="history">Package History</option>
-                                    <option value="message">Property Message</option>
-                                    <option value="testimonials">Testimonials</option>
-                                    <option value="agent">Manage Agent</option>
-                                    <option value="category">Blog Category</option>
-                                    <option value="post">Blog Post</option>
-                                    <option value="comment">Blog Comment</option>
-                                    <option value="smtp">SMTP Setting</option>
-                                    <option value="site">Site Setting</option>
-                                    <option value="role">Role & Permission</option>
+                                    <option value="type" {{$permission->group_name == "type" ? "selected" : ""}}>Property Type</option>
+                                    <option value="state" @if ($permission->group_name == 'state') selected @endif>State</option>
+                                    <option value="amenities" {{$permission->group_name == "amenities" ? "selected" : ""}}>Amenities</option>
+                                    <option value="property" {{$permission->group_name == "property" ? "selected" : ""}}>Property</option>
+                                    <option value="history" {{$permission->group_name == "history" ? "selected" : ""}}>Package History</option>
+                                    <option value="message" @if ($permission->group_name == 'message') selected @endif>Property Message</option>
+                                    <option value="testimonials" @if ($permission->group_name == 'testimonials') selected @endif>Testimonials</option>
+                                    <option value="agent" @if ($permission->group_name == 'agent') selected @endif>Manage Agent</option>
+                                    <option value="category" @if ($permission->group_name == 'category') selected @endif>Blog Category</option>
+                                    <option value="post" {{$permission->group_name == "post" ? "selected" : ""}}>Blog Post</option>
+                                    <option value="comment" {{$permission->group_name == "comment" ? "selected" : ""}}>Blog Comment</option>
+                                    <option value="smtp" {{$permission->group_name == "smtp" ? "selected" : ""}}>SMTP Setting</option>
+                                    <option value="site" {{$permission->group_name == "site" ? "selected" : ""}}>Site Setting</option>
+                                    <option value="role" {{$permission->group_name == "role" ? "selected" : ""}}>Role & Permission</option>
                                 </select>
+                                @error('group_name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                             </div>
                             <button type="submit" class="btn btn-primary me-2">Add Permission</button>
                         </form>
